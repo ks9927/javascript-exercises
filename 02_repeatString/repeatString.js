34a772ede035d416dcf5c3b7da924e1602434b79
KS <180429671+ks9927@users.noreply.github.com>
2025-03-21 15:58:07 -0400

    New string changed to empty string Also changed it to equal itself plus string inside for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,15 @@
 const repeatString = function(string, num) {
-    let newString = string;
+    let newString = '';
 
     if (num === 0) {
         return '';
-    }
+    }   
     for(i = 0; i < num; i++)
     {
+        newString += string;
         return newString;
     }
+
 };
 
 // Do this withought .repeat() method
