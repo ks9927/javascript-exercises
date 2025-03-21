198b20d011b0a2f63bf774fa01f734e7f3309d42
KS <180429671+ks9927@users.noreply.github.com>
2025-03-21 16:49:36 -0400

    Added variable to reverseString
    Variable is equal to the split string where the split is at a space

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Split string
+    let stringSplit = string.split("");
+    //Reverse it
+    
+    //Join it back together
 
 };
+
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
