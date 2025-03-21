8cc09fd1d6c4f8dce239d44564511e9729d2b46a
KS <180429671+ks9927@users.noreply.github.com>
2025-03-20 21:23:56 -0400

    Added if statement for if num is 0
    Then it returns an empty string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,17 @@
 const repeatString = function(string, num) {
     let newString = string;
+
+    if (num === 0) {
+        return '';
+    }
     for(i = 0; i < num; i++)
     {
         return newString.repeat(num);
     }
 };
 
-repeatString('hey', 3);
+// Repeats string 0 times..if num is 0 then return ''
+
+repeatString('hey', 0);
 // Do not edit below this line
 module.exports = repeatString;
