c6cd11d0aa21069894e408fdd9f4b2bf40983c22
KS <180429671+ks9927@users.noreply.github.com>
2025-03-20 21:05:22 -0400

    Added repeat method to return value in repeatStrin

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,7 +2,7 @@ const repeatString = function(string, num) {
     let newString = string;
     for(i = 0; i < num; i++)
     {
-        return newString;
+        return newString.repeat(num);
     }
 };
 
