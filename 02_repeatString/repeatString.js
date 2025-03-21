486c40258c8dc1d3fbc4d18c618f5a3ae5440433
KS <180429671+ks9927@users.noreply.github.com>
2025-03-20 20:14:29 -0400

    Edited repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = string;
+    for(i = 0; i < num; i++)
+    {
+        return newString;
+    }
 };
 
 // Do not edit below this line
