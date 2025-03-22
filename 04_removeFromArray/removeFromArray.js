5579855a7aeb6657d24434dfc9ff761a54417dff
KS <180429671+ks9927@users.noreply.github.com>
2025-03-22 19:17:59 -0400

    Finished removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,15 @@
-const removeFromArray = function(array, values) {
-   
-    
+const removeFromArray = function(array, ...values) {
+    let emptyArray = [];
+  //for each element in an array
+    array.forEach(element => {
+        if(!values.includes(element))
+        {
+            emptyArray.push(element);
+        }
+    });
+    return emptyArray;
 };
 
-removeFromArray([1, 2, 3, 4], 2, 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
