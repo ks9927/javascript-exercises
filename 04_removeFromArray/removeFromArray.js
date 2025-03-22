6489f0341a4ac3d1f55f070385a06e00f67f2daa
KS <180429671+ks9927@users.noreply.github.com>
2025-03-21 21:35:07 -0400

    Set up basic function Removed a single value from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function(array, value) {
+    //if array includes value, remove it
+    if (array.includes(value)) {
+     //find index of value
+    let index = array.indexOf(value);
+     array.splice(index, 1);
+    return array;
+    }
 };
 
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
