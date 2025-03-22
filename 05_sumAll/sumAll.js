a9bb8714c450d733c62266f6854f778163341daa
KS <180429671+ks9927@users.noreply.github.com>
2025-03-22 19:33:04 -0400

    Wrote sumAll function that works for 1st test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let totalSum = 0;
+    for(let i = num1; i < num2 + 1; i++)
+    {
+        totalSum += i;
+    }
+    return totalSum;
 };
 
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;
