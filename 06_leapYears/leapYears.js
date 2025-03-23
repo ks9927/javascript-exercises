9d35229b46b122c7a334bbfc8230f550c83f5768
KS <180429671+ks9927@users.noreply.github.com>
2025-03-22 21:35:18 -0400

    Finished leapYears function
    Moved things around and added not equal to 400 on one of them

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,25 +1,24 @@
 const leapYears = function(year) {
-    //if year is divisible by 4 return true
-    if(year % 4 === 0)
-    {
-        return true;
-    }
     //if year is divisible by 100 and divisible by 400 return true
+     if(year % 100 === 0 && year % 400 !== 0)
+    {
+        return false;
+    }
+    //if year is divisible by 100 return false
     else if(year % 100 === 0 && year % 400 === 0)
     {
         return true;
     }
-    //if year is divisible by 100 return false
-    else if(year % 100 === 0)
+   else if(year % 4 === 0)
     {
-        return false;
-    }
+         return true;
+     }
     else
     {
         return false;
     }
 };
 
-leapYears(1997);
+leapYears(1600);
 // Do not edit below this line
 module.exports = leapYears;
