a7ad51c3b88eb807fd962e0bf2465c7497364452
KS <180429671+ks9927@users.noreply.github.com>
2025-03-22 20:13:06 -0400

    Added conditional to sumAll
    If num1 is greater than num2 do the reverse of other for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,24 @@
 const sumAll = function(num1, num2) {
     let totalSum = 0;
+
+    if(num1 > num2)
+    {
+       for(let j = num1; j > num2 - 1; j--)
+       {
+            totalSum += j;
+       }
+       return totalSum;
+    }
+
+ else {
     for(let i = num1; i < num2 + 1; i++)
     {
         totalSum += i;
     }
+ }
     return totalSum;
 };
 
-sumAll(1, 4);
+sumAll(4, 1);
 // Do not edit below this line
 module.exports = sumAll;
