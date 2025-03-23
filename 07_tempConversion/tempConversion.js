317e5f90b3f01f99076b8df4e27c2065669695fc
KS <180429671+ks9927@users.noreply.github.com>
2025-03-22 21:53:01 -0400

    Created tempConversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(ctemp) {
+  //fahren to celcius
+  //f = (x − 32) × ⁠5/9 degrees c
+  //x is ctemp
+  let cDegree = (ctemp - 32) * 5/9;
+  return cDegree;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ftemp) {
+  //celcius to fahren
+  //c = (x * ⁠9/5 + 32) degrees f
+  //x is ftemp
+
+  //let fDegree = (fTemp * 9/5 + 32);
+  //return fDegree;
 };
 
+convertToCelsius(32);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
