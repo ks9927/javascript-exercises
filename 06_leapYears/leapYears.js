f919691a94c9bbf235a10c1a8546afd1c6651577
KS <180429671+ks9927@users.noreply.github.com>
2025-03-22 21:00:31 -0400

    Created basic leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //if year is divisible by 4 return true
+    if(year % 4)
+    {
+        return true;
+    }
+    //if year is divisible by 100 and divisible by 400 return true
+    else if(year % 100 && year % 400)
+    {
+        return true;
+    }
+    //if year is divisible by 100 return false
+    else if(year % 100)
+    {
+        return false;
+    }
+    else
+    {
+        return false;
+    }
 };
 
+leapYears(1996);
 // Do not edit below this line
 module.exports = leapYears;
