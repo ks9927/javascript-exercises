c5a173e6795c415fc0e2bfe4c8fe2be738c8750c
KS <180429671+ks9927@users.noreply.github.com>
2025-03-22 21:11:17 -0400

    Added equals 0 to conditionals
    This checks if number is divisible

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,16 +1,16 @@
 const leapYears = function(year) {
     //if year is divisible by 4 return true
-    if(year % 4)
+    if(year % 4 === 0)
     {
         return true;
     }
     //if year is divisible by 100 and divisible by 400 return true
-    else if(year % 100 && year % 400)
+    else if(year % 100 === 0 && year % 400 === 0)
     {
         return true;
     }
     //if year is divisible by 100 return false
-    else if(year % 100)
+    else if(year % 100 === 0)
     {
         return false;
     }
@@ -20,6 +20,6 @@ const leapYears = function(year) {
     }
 };
 
-leapYears(1996);
+leapYears(1997);
 // Do not edit below this line
 module.exports = leapYears;
